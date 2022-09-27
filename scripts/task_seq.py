8ff13cecd72e051695eeef6036fdfb70e4edf44a
asy62636 <57122243+asy62636@users.noreply.github.com>
2022-09-28 03:30:03 +0530

    Add files via upload

---
+++ scripts/task_seq.py
@@ -0,0 +1,3 @@
+task_seq_dict = {"Q.1":"E_stop_manual", "Q.2":'E_stop_wireless', "Q.3": 'Lane_keeping', 'Q.4':'Lane_keeping_at_intersections',
+"Q.4":'Left_Turn','Q.5':'Right_Turn','FV.4':'straight_lane_changing','FVII2':'merging','FVI1':'Curved_lane_handling', 'FVI2': 'Curved_lane_changing'}
+
